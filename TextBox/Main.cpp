db469d7c3c469d871bde060e609a879da6bd932d
Daniel <danieladler89@gmail.com>
2016-03-27 20:14:07 +0300

    textbox new

--- TextBox/Main.cpp
+++ TextBox/Main.cpp
@@ -10,6 +10,7 @@ DWORD fdwSaveOldMode;
 string myString;
 
 VOID KeyEventProc(KEY_EVENT_RECORD, Textbox t);
+VOID MouseEventProc(MOUSE_EVENT_RECORD, Textbox t);
 
 int main()
 {
@@ -23,7 +24,7 @@ int main()
 
 	while (counter++ <= 1000)
 	{
-		if (!ReadConsoleInput(hStdin,irInBuf,128,&cNumRead))
+		if (!ReadConsoleInput(hStdin, irInBuf, 128, &cNumRead))
 
 			fdwMode = ENABLE_WINDOW_INPUT;
 
@@ -35,6 +36,10 @@ int main()
 			{
 			case KEY_EVENT: // keyboard input 
 				KeyEventProc(irInBuf[i].Event.KeyEvent, box);
+				break;
+
+			case MOUSE_EVENT: // mouse input 
+				MouseEventProc(irInBuf[i].Event.MouseEvent, box);
 				break;
 
 			default:
@@ -51,8 +56,7 @@ VOID KeyEventProc(KEY_EVENT_RECORD ker, Textbox t)
 	int x = 0, y = 0, a, b;
 	if (GetAsyncKeyState(VK_RETURN) != 0)
 	{
-
-		cout << myString;
+		//nothing...
 	}
 	else if (GetAsyncKeyState(VK_BACK) != 0)
 	{
@@ -98,8 +102,28 @@ VOID KeyEventProc(KEY_EVENT_RECORD ker, Textbox t)
 	{
 		t.getPosition(x, y);
 		if (x >= 26)
+		{
 			t.position(25, 8);
+			myString.pop_back();
+		}
 		myString += ker.uChar.AsciiChar;
 		cout << ker.uChar.AsciiChar;
+	}
+}
+
+VOID MouseEventProc(MOUSE_EVENT_RECORD mer, Textbox t)
+{
+	switch (mer.dwEventFlags)
+	{
+	case 0:
+
+		if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED && mer.dwMousePosition.Y == 8 && mer.dwMousePosition.X > 7 && mer.dwMousePosition.X < 26)
+		{
+			t.position(mer.dwMousePosition.X, 8);
+		}
+		break;
+	default:
+		//Nothing...
+		break;
 	}
 }
